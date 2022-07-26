a11c347de9e022402743fb2b9885dc0e3977c744
josuenm <direct.josue@gmail.com>
2022-07-25 22:32:02 -0300

    Rota de user

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+
+
+const userRoute = express.Router()
+
+
+userRoute.post('/login', (request, response) => {
+
+})
+
+
+userRoute.post('/register', (request, response) => {
+
+})
+
+
+export { userRoute }
