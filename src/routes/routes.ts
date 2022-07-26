4be06a8564eed424a70a5504f46d333f593328ee
josuenm <direct.josue@gmail.com>
2022-07-25 22:09:59 -0300

    Controlador de rotas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { productsRoute } from './products.routes';
 
 
 
 const routes = express.Router()
 
-
+routes.use('/products', productsRoute)
 
 export { routes }
