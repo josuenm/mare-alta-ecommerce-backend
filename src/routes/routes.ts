0a816386bd222b5c6412348e353b541da1ca5f44
josuenm <direct.josue@gmail.com>
2022-07-25 22:31:44 -0300

    Rota de user

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { productsRoute } from './products.routes';
+import { userRoute } from './user.routes';
 
 
 
 const routes = express.Router()
 
 routes.use('/products', productsRoute)
+routes.use('/user', userRoute)
 
 export { routes }
